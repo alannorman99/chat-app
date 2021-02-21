2bbd7b3060b540a6719bb4f50cc401a49a4bdf14
Alan Norman <asnorman3604@eagle.fgcu.edu>
2021-02-20 22:40:56 -0500

    Added working connection between client and backend socket

--- server/index.js
+++ server/index.js
@@ -7,13 +7,22 @@ const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
 const app = express();
+
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+	cors: {
+		origin: "*",
+	},
+});
 
 io.on('connection', (socket) => {
 	console.log("We have a new connection");
 
-	socket.on('disconnect', () => {
+	socket.on('join', ({ name, room }, callback) => {
+		console.log(name, room);
+	});
+
+	socket.on('Disconnect', () => {
 		console.log("User has left");
 	});
 
